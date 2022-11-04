fd0247cdef5c0c48477603337ead6b173b895ece
Veridiana Maricato <maricato.veridiana@gmail.com>
2022-11-04 17:30:38 -0300

    jogo finalizado

---
+++ js/supertrunfo.js
@@ -0,0 +1,411 @@
+var carta1 = {
+    nome: "Davi Luiz",
+    imagem: 'https://www.istoedinheiro.com.br/wp-content/uploads/sites/17/2020/07/david-luiz-scaled.jpg',
+    atributos: {
+        agilidade: 9,
+        ataque: 9,
+        defesa:8
+    }
+}
+var carta2 = {
+    nome: "Richarlison",
+    imagem: 'https://conteudo.imguol.com.br/c/esporte/52/2021/12/07/richarlison-celebra-gol-marcado-pelo-everton-contra-o-arsenal-em-partida-do-campeonato-ingles-2021-22-1638889298649_v2_3x4.jpg',
+    atributos: {
+        agilidade: 9,
+        ataque: 10,
+        defesa:7
+    }
+}
+var carta3 = {
+    nome: "Gabigol",
+    imagem: 'https://conteudo.imguol.com.br/c/esporte/f3/2021/11/11/gabigol-comemora-gol-pelo-flamengo-contra-o-bahia-1636671290995_v2_3x4.jpg',
+    atributos: {
+        agilidade: 9,
+        ataque: 9,
+        defesa:8
+    }
+}
+var carta4 = {
+    nome: "Rodinei",
+    imagem: 'https://www.gremistas.net/wp-content/uploads/lateral-rodinei-em-campo-pelo-flamengo.jpg',
+    atributos: {
+        agilidade: 7,
+        ataque: 7,
+        defesa: 9
+    }
+}
+var carta5 = {
+    nome: "Allison",
+    imagem: 'https://cdn.folhape.com.br/img/pc/1100/1/dn_arquivo/2018/07/alisson.jpg',
+    atributos: {
+        agilidade: 8,
+        ataque: 5,
+        defesa:10
+    }
+}
+var carta6 = {
+    nome: "Fred",
+        imagem: 'https://www.lance.com.br/files/article_main/uploads/2022/02/20/621264d44765e.jpeg',
+    atributos: {
+        agilidade: 9,
+        ataque: 8,
+        defesa: 7
+    }
+}
+var carta7 = {
+    nome: "Filipe Luís",
+        imagem: 'https://imagens3.ne10.uol.com.br/blogsne10/torcedor/uploads/2018/06/filipe-luis-selecao-copa_748.jpg',
+    atributos: {
+        agilidade: 7,
+        ataque: 7,
+        defesa: 9
+    }
+}
+var carta8 = {
+    nome: "Danilo",
+        imagem: 'https://tntsports.com.br/__export/1628093434549/sites/esporteinterativo/img/2021/08/04/danilo.jpg_655186286.jpg',
+
+    atributos: {
+        agilidade: 7,
+        ataque: 7,
+        defesa:8
+    }
+}
+var carta9 = {
+    nome: "Antony",
+        imagem: 'https://www.ofutebolero.com.br/__export/1648604660158/sites/elfutboleromx/img/2022/03/29/antony-brasil.jpg_462530703.jpg',
+
+    atributos: {
+        agilidade: 8,
+        ataque: 9,
+        defesa:6
+    }
+}
+var carta10 = {
+    nome: "João Gomes",
+        imagem: 'https://colunadofla.com/wp-content/uploads/2022/08/joao-gomes-flamengo-x-sao-paulo-copa-do-brasil-gol-comemoracao.jpg',
+
+    atributos: {
+        agilidade: 10,
+        ataque: 8,
+        defesa:9
+    }
+}
+var carta11 = {
+    nome: "Everton Ribeiro",
+        imagem: 'https://s2.glbimg.com/YiRMOIdOhiecErdWtjhzk3vcmy8=/0x0:3000x1997/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2022/3/p/lgfh7KQxCySONfkiT0Nw/agif22101923211628.jpg',
+    atributos: {
+        agilidade: 8,
+        ataque: 7,
+        defesa:8
+    }
+}
+var carta12 = {
+    nome: "Pedro",
+        imagem: 'https://cms.somosfanaticos.com/__export/1659440338193/sites/fanaticos/img/2022/08/02/txcnico_pedex_e_clube_europeu_prepara_investida_pelo_atacante_pedrox_do_flamengo.jpg_1159711837.jpg',
+    atributos: {
+        agilidade: 10,
+        ataque: 10,
+        defesa: 6
+    }
+}
+var carta13 = {
+    nome: "Nino",
+    imagem: 'https://static-wp-tor15-prd.torcedores.com/wp-content/uploads/2022/06/nino-fluminense-2022-1.png',
+    atributos: {
+        agilidade: 8,
+        ataque: 7,
+        defesa: 9
+    }
+}
+var carta14 = {
+    nome: "Léo Ortiz",
+    imagem: 'https://tntsports.com.br/__export/1584141041514/sites/esporteinterativo/img/2020/03/13/leo_ortiz.jpg_1582050061.jpg',
+    atributos: {
+        agilidade: 9,
+        ataque: 8,
+        defesa: 9
+    }
+}
+var carta15 = {
+    nome: "Felipe",
+    imagem: 'https://conteudo.imguol.com.br/c/esporte/0b/2021/11/20/felipe-comemora-gol-do-atletico-de-madri-contra-o-osasuna-pelo-campeonato-espanhol-1637436934686_v2_3x4.jpg',
+    atributos: {
+        agilidade: 10,
+        ataque: 8,
+        defesa: 9
+    }
+}
+var carta16 = {
+    nome: "Bremer",
+    imagem: 'https://www.juvefc.com/wp-content/uploads/2022/08/bremer3.jpg',
+    atributos: {
+        agilidade: 10,
+        ataque: 9,
+        defesa: 9
+    }
+}
+var carta17 = {
+    nome: "Caio Henrique",
+    imagem: 'https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/blt893393130908aea8/621fa96a051dd27a431d23c6/Caio_Henrique_AS_Monaco.png',
+    atributos: {
+        agilidade: 9,
+        ataque: 8,
+        defesa: 9
+    }
+}
+var carta18 = {
+    nome: "Matheus Cunha",
+    imagem: 'https://conteudo.imguol.com.br/c/esporte/40/2020/01/23/matheus-cunha-comemora-gol-pela-selecao-brasileira-contra-o-uruguai-no-pre-olimpico-1579834618735_v2_4x3.jpg',
+    atributos: {
+        agilidade: 10,
+        ataque: 10,
+        defesa: 6
+    }
+}
+var carta19 = {
+    nome: "Firmino",
+    imagem: 'https://www.lance.com.br/files/article_main/files/crop/uploads/2022/02/16/620d8d766ac72.r_1658771020390.37-62-1226-854.jpeg',
+    atributos: {
+        agilidade: 10,
+        ataque: 10,
+        defesa: 7
+    }
+}
+var carta20 = {
+    nome: "Rodrygo",
+    imagem: 'https://bolavip.com/__export/1651777754821/sites/bolavip/img/2022/05/05/real_madrid_rodrygo_heroe_champions_league_2022.jpg_1159711837.jpg',
+    atributos: {
+        agilidade: 10,
+        ataque: 10,
+        defesa: 6
+    }
+}
+var carta21 = {
+    nome: "Weverton",
+    imagem: 'https://img.r7.com/images/weverton-11082022022900045?dimensions=771x420',
+    atributos: {
+        agilidade: 9,
+        ataque: 3,
+        defesa: 10
+    }
+}
+var carta22 = {
+    nome: "Alex Telles",
+    imagem: 'https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/blt48b3f82a76b547ae/61a9c0714d33f20e3e89f60d/GettyImages-1344026352.jpeg?auto=webp&format=jpg&quality=100',
+    atributos: {
+        agilidade: 9,
+        ataque: 8,
+        defesa: 9
+    }
+}
+var carta23 = {
+    nome: "Thiago Silva",
+    imagem: 'https://conteudo.imguol.com.br/c/esporte/01/2022/02/12/thiago-silva-recebe-o-premio-de-melhor-jogador-do-mundial-de-clubes-1644694414958_v2_450x600.jpg',
+    atributos: {
+        agilidade: 9,
+        ataque: 8,
+        defesa: 10
+    }
+}
+var carta24 = {
+    nome: "Marquinhos",
+    imagem: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuYLDvFT3w_mrxfZS5VuOqgdacuuqFmpa-ViEujYnLl3iNbx2Zrlz3ylKyZaICY4OjhSk&usqp=CAU',
+    atributos: {
+        agilidade: 9,
+        ataque: 8,
+        defesa: 10
+    }
+}
+var carta25 = {
+    nome: "Éder Militão",
+    imagem: 'https://i0.wp.com/portalaltadefinicao.com/wp-content/uploads/2022/10/militao-selecao-brasileira.jpg?resize=750%2C422&ssl=1',
+    atributos: {
+        agilidade: 10,
+        ataque: 8,
+        defesa: 9
+    }
+} 
+var carta26 = {
+    nome: "Casemiro",
+    imagem: 'https://www.ofutebolero.com.br/__export/1629065837628/sites/elfutboleromx/img/2021/08/15/4107946.jpg_1720385577.jpg',
+    atributos: {
+        agilidade: 10,
+        ataque: 8,
+        defesa: 10
+    }
+}
+
+
+var listaCartas = [carta1, carta2, carta3, carta4, carta5, carta6, carta7, carta8, carta9, carta10, carta11, carta12, carta13, carta14, carta15, carta16, carta17, carta18, carta19, carta20, carta21, carta22, carta23, carta24, carta25, carta26]
+
+atualizaQuantidadeCartas()
+var cartaComputador;
+var cartaJogador;
+var pontosJogador = 0;
+var pontosComputador = 0;
+
+function atualizaPlacar(){
+    var divPlacarJogador = document.getElementById('pontos-jogador');
+    divPlacarJogador.innerHTML = "Você: " + pontosJogador
+    var divPlacarComputador = document.getElementById('pontos-computador');
+    divPlacarComputador.innerHTML = "Computador: " + pontosComputador
+}
+atualizaPlacar()
+
+function sortearCarta(){
+    var numeroCartaComputador = parseInt(Math.random() * listaCartas.length);
+    cartaComputador = listaCartas[numeroCartaComputador] 
+    listaCartas.splice(numeroCartaComputador, 1)
+
+    var numeroCartaJogador = parseInt(Math.random() * listaCartas.length);
+    cartaJogador = listaCartas[numeroCartaJogador] 
+    listaCartas.splice(numeroCartaJogador, 1)
+
+    document.getElementById('btnSortear').disabled = true
+    document.getElementById('btnProximaRodada').disabled = true
+    desativaBotao()
+
+    exibirCartaJogador()
+}
+
+function exibirCartaJogador(){
+    document.querySelector('.carta-jogador').classList.remove('hidden')
+
+    var divNomeJogador = document.getElementById('nome-jogador')
+    var divFotoJogador = document.getElementById('carta-jogador__foto')
+    var divTextoJogador = document.getElementById('texto')
+ 
+    divFotoJogador.style.backgroundImage = `url(${cartaJogador.imagem})`
+ 
+    var opcoesTextos = "";
+    for (var atributo in cartaJogador.atributos) {
+        opcoesTextos +=
+          "<input type='radio' name='atributo' value=" +
+          atributo +
+          " >" +
+          atributo +
+          " " +
+          "<br>";
+      }
+
+
+    divNomeJogador.innerHTML = cartaJogador.nome
+    divTextoJogador.innerHTML = opcoesTextos
+
+    document.getElementById("btnSortear").disabled = true;
+}
+
+function exibirCartaComputador(){
+    document.querySelector('.carta-computador').classList.remove('hidden')
+
+    var divNomeComputador = document.getElementById('nome-computador')
+    var divFotoComputador = document.getElementById('carta-computador__foto')
+    var divTextoComputador = document.getElementById('texto-computador')
+ 
+    divFotoComputador.style.backgroundImage = `url(${cartaComputador.imagem})`
+ 
+    var opcoesTextosComputador = "";
+    for (var atributo in cartaComputador.atributos) {
+        opcoesTextosComputador +=
+          "<input class='hidden' type='radio' name='atributo' value=" +
+          atributo +
+          " >" +
+          atributo +
+          " " +
+          "<br>";
+      }
+
+    divNomeComputador.innerHTML = cartaComputador.nome
+    divTextoComputador.innerHTML = opcoesTextosComputador
+    document.getElementById("btnSortear").disabled = true;
+}
+
+function desativaBotao(){
+    document.getElementById('btnJogar').disabled = false
+}
+
+function obtemAtributoSelecionado() {
+    var radioAtributos = document.getElementsByName("atributo");
+  
+    for (var i = 0; i < radioAtributos.length; i++) {
+      if (radioAtributos[i].checked == true) {        
+        return radioAtributos[i].value;        
+      }
+      document.querySelector('#btnJogar').addEventListener('click', desativaBotao())
+    }   
+    
+  }
+
+  function jogar() {
+    console.log(atributoSelecionado)
+    document.getElementById("btnProximaRodada").disabled = false;
+    document.querySelector('.resultado').classList.remove('hidden')
+    atualizaQuantidadeCartas()
+    
+    var atributoSelecionado = obtemAtributoSelecionado();
+    var divResultado = document.getElementById("resultado");
+    var htmlResultado;
+  
+    var valorAtributoSelecionadoJogador =
+      cartaJogador.atributos[atributoSelecionado];
+    var valorAtributoSelecionadoComputador =
+      cartaComputador.atributos[atributoSelecionado];
+  
+    if (valorAtributoSelecionadoJogador > valorAtributoSelecionadoComputador) {
+      htmlResultado = "<p class='resultado-final'>VENCEU, CAMPEAO!</p>";
+      pontosJogador += 1;
+    } else if (
+      valorAtributoSelecionadoComputador > valorAtributoSelecionadoJogador
+    ) {
+      htmlResultado = "<p class='resultado-final'>XIII... PERDEU!</p>";
+      pontosComputador += 1;
+    } else {
+      htmlResultado = "<p class='resultado-final'>EMPATE!</p>";
+    }
+  
+    if (listaCartas.length == 0) {
+      alert("ACABOU O JOGO!");
+      document.querySelector('.jogar-novamente').classList.remove('hidden')
+      document.getElementById('btnProximaRodada').disabled = true
+
+      if (pontosComputador > pontosJogador) {
+        htmlResultado =
+          '<p class="resultado-final">SINTO MUITO... MAIS SORTE NA PROXIMA!</p>';
+      } else if (pontosComputador < pontosJogador) {
+        htmlResultado =
+          '<p class="resultado-final">PARABÉNS, VOCÊ VENCEU!</p>';
+      } else {
+        htmlResultado =
+          '<p class="resultado-final">EMPATOU!</p>';
+      }
+    }
+  
+    divResultado.innerHTML = htmlResultado;
+    exibirCartaComputador();
+    atualizaPlacar();
+  
+    document.getElementById("btnSortear").disabled = true;
+    document.getElementById("btnJogar").disabled = true;
+  }
+
+function proximaRodada() {
+    document.querySelector('.carta-computador').classList.add('hidden')
+    document.querySelector('.carta-jogador').classList.add('hidden')
+
+  
+    document.getElementById("btnSortear").disabled = false;
+    document.getElementById("btnJogar").disabled = true;
+    document.getElementById("btnProximaRodada").disabled = true;
+  
+    document.querySelector('.resultado').classList.add('hidden')
+  }
+
+  function atualizaQuantidadeCartas() {
+    var divQuantidadeCartas = document.getElementById("quantidade-cartas");
+    divQuantidadeCartas.innerHTML = listaCartas.length;
+  }
+
+  function jogarNovamente(){
+    window.location.reload(true);
+  }
